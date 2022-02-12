e6282d32593faa80646dc29ef8d962bd667176af
rptseng <tseng_r@hotmail.com>
2022-02-12 05:37:19 -0500

    updated linear regression model

--- MechaCar_Challenge/MechaCarChallenge.R
+++ MechaCar_Challenge/MechaCarChallenge.R
@@ -2,5 +2,5 @@ library(dplyr)
 
 mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
+mechacar_mpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
+summary(mechacar_mpg_model)
