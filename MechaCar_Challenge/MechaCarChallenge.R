cbfdc3a1c4bebbe4b3e84d05e4966f23aed34eee
rptseng <tseng_r@hotmail.com>
2022-02-12 06:40:40 -0500

    created summary statistics for PSI

--- MechaCar_Challenge/MechaCarChallenge.R
+++ MechaCar_Challenge/MechaCarChallenge.R
@@ -4,3 +4,7 @@ mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactor
 
 mechacar_mpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
 summary(mechacar_mpg_model)
+
+suspension_coil <- read.csv(file="Suspension_Coil.csv", check.names=F, stringsAsFactors = F)
+total_summary <- suspension_coil %>% summarize(Mean_Mileage=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI)) #create summary table with multiple columns
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_Mileage=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
